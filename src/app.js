ebc1f4eb2e2c995e019e58d1ebe44c1811c88f13
ttilley02 <ttilley02@gmail.com>
2020-06-09 20:24:48 -0400

    patch updated

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const jsonParser = express.json();
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const ArticlesService = require('./articles/articles-service');
+const articlesRouter = require('./articles/articles-router')
 
 const app = express()
 
@@ -11,24 +14,27 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.use('/api/articles', articlesRouter)
 
 
 app.get('/', (req, res) => {
-       res.send('Hello, world!')
-     })
+       res.send('Hello, world!');
+     });
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
+    let response;
     if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
+        response = { error: { message: 'server error' } };
     } else {
         console.error(error)
-        response = { message: error.message, error }
+        response = { message: error.message, error };
     }
-    res.status(500).json(response)
-    })
+    res.status(500).json(response);
+    });
 
-module.exports = app
+
+module.exports = app;
